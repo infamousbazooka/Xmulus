76794f92831d3017d2242103962a6246b119c1a2
infamousbazooka <magnusfernandes1295@gmail.com>
2017-01-09 01:25:39 +0530

    Movies page. Search Movies.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,3 +9,30 @@ $(document).ready(function() {
   $('.list_slider li').css('width', $('.main').width()/4);
   $('.list_slider ul').css('width', $('.main').width()*5 + 80);
 });
+
+$('#searchform').submit(function(e){
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=fa1ad33c2c7b13939445ce18e5209ee0&language=en-US&query="
+    + $('#search').val() + "&page=1&include_adult=false";
+
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: function(data){
+        for (var i = 0; i < data.results.length; i++) {
+          console.log(backdrop);
+          if (data.results[i].backdrop_path) {
+            var backdrop = 'https://image.tmdb.org/t/p/w500/' + data.results[i].backdrop_path;
+          } else{
+            backdrop = '/images/backdrop.jpg';
+          }
+          $('#searchlist').append('<li class="col s4"><div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + backdrop
+          +'"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'
+          + data.results[i].title + '<i class="material-icons right">more_vert</i></span><p><a href="/Dashboard/Movies/'
+          + data.results[i].id
+          + '">Check this Movie</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span><p>'
+          + data.results[i].overview + '</p></div></div></li>');
+        }
+      }
+    });
+    e.preventDefault();
+});
